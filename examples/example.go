7e1cf2251c509d78019fbe35b62117411e170ee7
mirkobrombin <send@mirko.pm>
2023-04-30 18:19:35 +0200

    add examples

---
+++ examples/example.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/containers/storage/pkg/reexec"
+	"github.com/vanilla-os/prometheus"
+)
+
+func main() {
+	if reexec.Init() {
+		return
+	}
+
+	// -----------------------------
+	fmt.Println("Building image from a Containerfile...")
+
+	pmt, err := prometheus.NewPrometheus("storage", "overlay")
+	if err != nil {
+		panic(err)
+	}
+
+	image, err := pmt.BuildContainerFile("Containerfile", "example")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Image built: %s\n", image.ID)
+
+	// -----------------------------
+
+	fmt.Println("Mounting top layer...")
+
+	mountPath, err := pmt.MountImage(image.TopLayer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Mounted at: %s\n", mountPath)
+}
